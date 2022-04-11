fc8d00f6fb8322557a714e5b4c9275996205fb2e
francorodrigofigueroa <francorodrigofigueroa@gmail.com>
2022-04-10 21:36:26 -0300

    hice hasta los graficos de conocimientos

--- Proyecto_FRF/src/app/app.module.ts
+++ Proyecto_FRF/src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './conponents/header/header.component';
+import { LogoAPComponent } from './conponents/logo-ap/logo-ap.component';
+import { SocialComponent } from './conponents/social/social.component';
+import { BannerComponent } from './conponents/banner/banner.component';
+import { AcercaDeComponent } from './conponents/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './conponents/experiencia/experiencia.component';
+import { EducacionComponent } from './conponents/educacion/educacion.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { HysComponent } from './conponents/hys/hys.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoAPComponent,
+    SocialComponent,
+    BannerComponent,
+    AcercaDeComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    HysComponent,
+       
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
   ],
   providers: [],
   bootstrap: [AppComponent]
